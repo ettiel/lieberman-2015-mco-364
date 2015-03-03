3efde0488a38797f63539eb0b6e7fe1c9535e26d
ettiel <ettieel@yahoo.com>
2015-03-03 01:42:24 -0500

    snake update

--- src/lieberman/snakeNew/SnakePanel.java
+++ src/lieberman/snakeNew/SnakePanel.java
@@ -105,12 +105,12 @@ public class SnakePanel extends JPanel implements Runnable {
 	public void paint(Graphics g) {
 		g.clearRect(0, 0, WIDTH, HEIGHT);
 		g.setColor(Color.BLUE);
-		for (int i = 0; i < WIDTH / 10; i++) {
-			g.drawLine(i * 10, 0, i * 10, HEIGHT);
-		}
-		for (int i = 0; i < HEIGHT / 10; i++) {
-			g.drawLine(0, i * 10, WIDTH, i * 10);
-		}
+//		for (int i = 0; i < WIDTH / 10; i++) {
+//			g.drawLine(i * 10, 0, i * 10, HEIGHT);
+//		}
+//		for (int i = 0; i < HEIGHT / 10; i++) {
+//			g.drawLine(0, i * 10, WIDTH, i * 10);
+//		}
 
 		for (int i = 0; i < snake.size(); i++) {
 			snake.get(i).draw(g);
