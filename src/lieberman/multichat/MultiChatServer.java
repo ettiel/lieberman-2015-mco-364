2ba40955f42d0bf36632b3e4fd1645d8784e271a
ettiel <ettieel@yahoo.com>
2015-05-04 22:12:12 -0400

    no message

--- src/lieberman/multichat/MultiChatServer.java
+++ src/lieberman/multichat/MultiChatServer.java
@@ -7,24 +7,57 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class MultiChatServer {
+public class MultiChatServer implements ReaderListener {
 
-	public static void main(String[] args) throws IOException{
-		
-		ServerSocket serverSocket = new ServerSocket(1234);
-		List<Socket> sockets = new ArrayList<Socket>();
-		
-		LinkedBlockingQueue<String> messages = new LinkedBlockingQueue<String>(); // will block at the take method and wait for there to be something to take
-		
-		while (true) {
-			Socket socket = serverSocket.accept();
-			sockets.add(socket);
-		
-		
-		
+	private Socket socket;
+	private LinkedBlockingQueue<String> messages;
+	private List<Socket> sockets;
+	private WriterThread writer;
+
+	public MultiChatServer() {
+		messages = new LinkedBlockingQueue<String>(); // will block at the take
+														// method and wait for
+														// there to be something
+														// to take
+		sockets = new ArrayList<Socket>();
+		writer = new WriterThread(messages, sockets);
+		writer.start();
+
+		try {
+
+			ServerSocket serverSocket = new ServerSocket(1234);
+
+			while (true) {
+				Socket socket = serverSocket.accept();
+				sockets.add(socket);
+				ReaderThread thread = new ReaderThread(socket, this);
+				thread.start();
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
 		}
-		
-		
+
 	}
-	
+
+	public static void main(String[] args) throws IOException {
+
+		MultiChatServer MCServer = new MultiChatServer();
+
+	}
+
+	@Override
+	public void onLineRead(String line) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCloseSocket(Socket socket) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
